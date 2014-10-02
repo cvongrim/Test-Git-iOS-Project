803a18bbd793fb1aaf575861ecdfa9864317eb6a
Colin von Grimmenstein <colinvong@gmail.com>
2014-10-02 14:59:07 -0400

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Colin von Grimmenstein on 10/2/14.
+//  Copyright (c) 2014 xiik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
