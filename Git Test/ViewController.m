984b032546982baa419644e9dbe6c25a1baa5cc7
Colin von Grimmenstein <colinvong@gmail.com>
2014-10-02 14:47:19 -0400

    Initial Commit

---
+++ Git Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Git Test
+//
+//  Created by Colin von Grimmenstein on 10/2/14.
+//  Copyright (c) 2014 xiik. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
